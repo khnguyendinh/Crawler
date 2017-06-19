1c28ec077f8fd99dd6621c298ff964bd788d661d
KHOAND_ADMIN <khnguyendinh@gmail.com>
2017-06-19 11:02:58 +0700

    System.out.println(tenPhim);         System.out.println(idPhim);         System.out.println("urlRutGon "+urlRutGon);         System.out.println(ngayChieu);         System.out.println("idKhungGio "+idKhungGio);         System.out.println("khungGio "+khungGio);         System.out.println(ngayKhoiChieu);         System.out.println(anhDaiDien);         System.out.println(thoiLuong);         System.out.println(daoDien);         System.out.println(dienVien);         System.out.println(quocGia);         System.out.println(theLoai);         System.out.println(trailer);         System.out.println(noiDung);

--- src/Main.java
+++ src/Main.java
@@ -15,11 +15,12 @@ public class Main {
         Main main = new Main();
         String myFilm = "https://chieuphimquocgia.com.vn/vet-co-phieu-luu-ky-(2d-lt)---p-chi-tiet-film-7403";
         String myRoom = "https://chieuphimquocgia.com.vn/PlanScreenings/BookTicket?pId=144494";
-        main.processPage(myFilm,myRoom);
+        String linkRap = "https://chieuphimquocgia.com.vn";
+        main.processPage(myFilm,myRoom,linkRap);
 
     }
 
-    private void processPage(String urlFilm,String urlRoom) throws IOException {
+    private void processPage(String urlFilm,String urlRoom,String linkRap) throws IOException {
         Document docFilm = Jsoup.connect(urlFilm).get();
         Elements elements = docFilm.getElementsByClass("wrapper");
         Element element = elements.get(2);
@@ -29,14 +30,15 @@ public class Main {
 
         String idRap;
         String tenRap;
-        String urlRap;
+        String urlRap ;
         String tenPhim = content_film.select("a[href]").get(0).text().toString();
         String tenPhimTiengAnh;
         String idPhim = content_film.select("p").get(7).select("a[href]").get(0).attr("href").replace("/PlanScreenings/Sessiontimes?filmId=","");
-        String urlRutGon;
+        String urlRutGon = linkRap + docFilm.getElementsByClass("screen_showtime").select("a[href]").get(0).attr("href");
         String ngayChieu = docFilm.getElementsByClass("showtime").text();
-        String idKhungGio;
-        String khungGio;
+        ngayChieu = ngayChieu.substring(ngayChieu.indexOf("-")+1,ngayChieu.indexOf("-")+11);
+        String idKhungGio = docFilm.getElementsByClass("screen_showtime").select("a[href]").get(0).attr("href").replace("/PlanScreenings/BookTicket?pId=","");
+        String khungGio = docFilm.getElementsByClass("screen_showtime").select("a[href]").get(0).text();
         String ngayKhoiChieu = content_film.select("p").get(5).text().replace("Khởi chiếu :","");
         String anhDaiDien = content_film.select("img[src$=.jpg]").get(0).absUrl("src").toString();
         String thoiLuong = content_film.select("p").get(1).text().replace("Thời lượng: ","");
@@ -57,7 +59,10 @@ public class Main {
 
         System.out.println(tenPhim);
         System.out.println(idPhim);
+        System.out.println("urlRutGon "+urlRutGon);
         System.out.println(ngayChieu);
+        System.out.println("idKhungGio "+idKhungGio);
+        System.out.println("khungGio "+khungGio);
         System.out.println(ngayKhoiChieu);
         System.out.println(anhDaiDien);
         System.out.println(thoiLuong);
