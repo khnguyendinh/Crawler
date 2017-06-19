e617970ef2b7e5dca83277f5ad8fe0d75050cf64
KHOAND_ADMIN <khnguyendinh@gmail.com>
2017-06-19 10:38:02 +0700

    System.out.println(tenPhim);         System.out.println(idPhim);         System.out.println(ngayChieu);         System.out.println(ngayKhoiChieu);         System.out.println(anhDaiDien);         System.out.println(thoiLuong);         System.out.println(daoDien);         System.out.println(dienVien);         System.out.println(quocGia);         System.out.println(theLoai);         System.out.println(trailer);         System.out.println(noiDung);

--- src/Main.java
+++ src/Main.java
@@ -4,29 +4,70 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 
 public class Main {
-    String myURL = "https://www.cgv.vn/default/cinox/reserve/cgv-iph-ha-noi";
+
     public Main() {
     }
 
     public static void main(String[] args) throws IOException {
         Main main = new Main();
-        main.processPage("myURL");
+        String myFilm = "https://chieuphimquocgia.com.vn/vet-co-phieu-luu-ky-(2d-lt)---p-chi-tiet-film-7403";
+        String myRoom = "https://chieuphimquocgia.com.vn/PlanScreenings/BookTicket?pId=144494";
+        main.processPage(myFilm,myRoom);
 
     }
 
-    private void processPage(String url) throws IOException {
-//        Document doc = Jsoup.connect(url).get();
-//        Document doc = Jsoup.connect("http://en.wikipedia.org/").get();
-//        Elements newsHeadlines = doc.select("#mp-itn b a");
-//        System.out.println(doc.toString());
-//        for (Element element: newsHeadlines) {
-//            System.out.println("Main.processPage "+element.toString());
-//        }
-        String html = "<div><p>Lorem ipsum.</p>";
-        Document doc = Jsoup.parseBodyFragment(html);
-        Element body = doc.body();
-        System.out.println("Main.processPage "+body.toString());
+    private void processPage(String urlFilm,String urlRoom) throws IOException {
+        Document docFilm = Jsoup.connect(urlFilm).get();
+        Elements elements = docFilm.getElementsByClass("wrapper");
+        Element element = elements.get(2);
+        Element content_film = element.getElementsByClass("content_movie").get(0);
+
+        Document docRoom = Jsoup.connect(urlRoom).get();
+
+        String idRap;
+        String tenRap;
+        String urlRap;
+        String tenPhim = content_film.select("a[href]").get(0).text().toString();
+        String tenPhimTiengAnh;
+        String idPhim = content_film.select("p").get(7).select("a[href]").get(0).attr("href").replace("/PlanScreenings/Sessiontimes?filmId=","");
+        String urlRutGon;
+        String ngayChieu = docFilm.getElementsByClass("showtime").text();
+        String idKhungGio;
+        String khungGio;
+        String ngayKhoiChieu = content_film.select("p").get(5).text().replace("Khởi chiếu :","");
+        String anhDaiDien = content_film.select("img[src$=.jpg]").get(0).absUrl("src").toString();
+        String thoiLuong = content_film.select("p").get(1).text().replace("Thời lượng: ","");
+        String daoDien = content_film.select("p").get(3).text().replace("Đạo diễn:","");
+        String dienVien = content_film.select("p").get(2).text().replace("Diễn viên: ","");
+        String ngonNgu;
+        String quocGia = content_film.select("p").get(4).text().replace("Xuất xứ:","");
+        String theLoai = content_film.select("p").get(0).text().replace("Loại phim:","");
+        String trailer = content_film.select("p").get(7).getElementsByClass("trainer").attr("href");
+        String noiDung = content_film.select("p").get(6).text().replace("Xuất xứ:","");
+        String idKhungGioClick;
+        String phongChieu;
+        String urlChiTietPhim = "";
+        String list_trailer;
+        int idLocation;
+        String tinhThanh;
+        String tenPhimChuanVN;
+
+        System.out.println(tenPhim);
+        System.out.println(idPhim);
+        System.out.println(ngayChieu);
+        System.out.println(ngayKhoiChieu);
+        System.out.println(anhDaiDien);
+        System.out.println(thoiLuong);
+        System.out.println(daoDien);
+        System.out.println(dienVien);
+        System.out.println(quocGia);
+        System.out.println(theLoai);
+        System.out.println(trailer);
+        System.out.println(noiDung);
+
+
     }
 }
